61e1763235b31675f9d2749e147b5f832d7f1475
Andi-228 <danusenko@.com>
2021-06-23 21:16:24 +0300

    added all models and rotate

---
+++ Sheepy/Assets/RW/Scripts/Rotator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [Header("Rotate Propperty")]
+    [SerializeField] Vector3 rotateDirection;
+    [Range(-20f, 20f)][SerializeField] float rotateSpeed;
+³
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateDirection * rotateSpeed * Time.deltaTime);
+    }
+}
